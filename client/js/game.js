fabc12883220be237e732f7faf0349dc9b68a3d8
Copephobia <copephobia@gmail.com>
2016-10-04 21:51:30 -0400

    Move player away from door
    Move the player away from the door so they have an easier time
    returning.

--- client/js/game.js
+++ client/js/game.js
@@ -945,12 +945,19 @@ function(InfoManager, BubbleManager, Renderer, Map, Animation, Sprite, AnimatedT
                 
                     if(!self.player.hasTarget() && self.map.isDoor(x, y)) {
                         var dest = self.map.getDoorDestination(x, y);
+						var desty = dest.y;
+						
+						if (dest.orientation === Types.Orientations.UP) {
+							desty--;
+						} else if (dest.orientation === Types.Orientations.DOWN) {
+							desty++;
+						}
                     
-                        self.player.setGridPosition(dest.x, dest.y);
+                        self.player.setGridPosition(dest.x, desty);
                         self.player.nextGridX = dest.x;
-                        self.player.nextGridY = dest.y;
+                        self.player.nextGridY = desty;
                         self.player.turnTo(dest.orientation);
-                        self.client.sendTeleport(dest.x, dest.y);
+                        self.client.sendTeleport(dest.x, desty);
                         
                         if(self.renderer.mobile && dest.cameraX && dest.cameraY) {
                             self.camera.setGridPosition(dest.cameraX, dest.cameraY);
